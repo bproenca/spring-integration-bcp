fee3489619ff6dd276b85ea262f6c59092721bf0
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-01-16 21:23:48 -0300

    output channel

--- src/main/java/br/com/bcp/integration/PrintService.java
+++ src/main/java/br/com/bcp/integration/PrintService.java
@@ -1,14 +1,17 @@
 package br.com.bcp.integration;
 
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 
 public class PrintService {
 	
-	public void print(Message<String> message) {
+	public Message<String> print(Message<String> message) {
 		System.out.println(message.getPayload());
 		MessageHeaders headers = message.getHeaders();
 		System.out.println(headers);
+		
+		return MessageBuilder.withPayload("Return Message").setHeader("aa", "bb").build();
 	}
 
 }
--- src/main/java/br/com/bcp/integration/SpringIntegrationBcpApplication.java
+++ src/main/java/br/com/bcp/integration/SpringIntegrationBcpApplication.java
@@ -3,15 +3,20 @@ package br.com.bcp.integration;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.GenericMessage;
 
 @SpringBootApplication
@@ -19,17 +24,34 @@ import org.springframework.messaging.support.GenericMessage;
 @ImportResource("integration-context.xml")
 public class SpringIntegrationBcpApplication implements ApplicationRunner{
 
+	@Autowired
+	@Qualifier("inputChannel")
+	private DirectChannel inputChannel;
+	
+	@Autowired
+	@Qualifier("outputChannel")
+	private DirectChannel outputChannel;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntegrationBcpApplication.class, args);
 	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		
+		outputChannel.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				new PrintService().print((Message<String>) message);
+			}
+		});
+		
 		Message<String> message = MessageBuilder
 				.withPayload("Message builder")
 				.setHeader("headerName", "headerValue").build();
-		PrintService service = new PrintService();
-		service.print(message);
+		
+		
+		inputChannel.send(message);
 	}
 
 }
