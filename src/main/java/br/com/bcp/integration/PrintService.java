d591560e73881ac6ded0ee5be703318dd3c9878d
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-01-24 17:54:07 -0300

    failover

--- src/main/java/br/com/bcp/integration/PrintService.java
+++ src/main/java/br/com/bcp/integration/PrintService.java
@@ -1,15 +1,12 @@
 package br.com.bcp.integration;
 
-import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
 
 public class PrintService {
 	
-	public Message<String> print(Message<String> message) {
-		System.out.println("[1]" + message.getPayload());
-		int messageNumber = (int) message.getHeaders().get("messageNumber");
-		return MessageBuilder.withPayload("Sending reply for message " + messageNumber).setHeader("aa", "bb").build();
+	public void print(Message<String> message) {
+		throw new RuntimeException("## this is an error " + message.getPayload().substring(message.getPayload().length()-1) + " ##");
+		//System.out.println(message.getPayload());
 	}
 
 }
--- src/main/java/br/com/bcp/integration/SpringIntegrationBcpApplication.java
+++ src/main/java/br/com/bcp/integration/SpringIntegrationBcpApplication.java
@@ -1,9 +1,5 @@
 package br.com.bcp.integration;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Future;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -28,23 +24,10 @@ public class SpringIntegrationBcpApplication implements ApplicationRunner{
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		List<Future<Message<String>>> futures = new ArrayList<Future<Message<String>>>();
-		
 		for (int i = 0; i < 10; i++) {
-			Message<String> message = MessageBuilder
-					.withPayload("Printing message payload for " + i)
-					.setHeader("messageNumber", i)
-					.setPriority(i)
-					.build();
-			System.out.println("[0] Sending message " + i);
-			futures.add(gateway.print(message));
-		}
-		
-		for (Future<Message<String>> future : futures) {
-			System.out.println("[2]" + future.get().getPayload());
-		}
-		
-		System.out.println("done");
+			Message<String> message = MessageBuilder.withPayload("Printing message payload for " + i).build();
+			gateway.print(message);
+		}		
 	}
 
 }
