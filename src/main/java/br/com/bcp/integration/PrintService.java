6d41ca7247286b6aa5ac36dd72b4347ad76d3a4c
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-01-31 13:31:10 -0300

    pub sub channel

--- src/main/java/br/com/bcp/integration/PrintService.java
+++ src/main/java/br/com/bcp/integration/PrintService.java
@@ -5,8 +5,8 @@ import org.springframework.messaging.Message;
 public class PrintService {
 	
 	public void print(Message<String> message) {
-		throw new RuntimeException("## this is an error " + message.getPayload().substring(message.getPayload().length()-1) + " ##");
-		//System.out.println(message.getPayload());
+		//throw new RuntimeException("## this is an error " + message.getPayload().substring(message.getPayload().length()-1) + " ##");
+		System.out.println(message.getPayload());
 	}
 
 }
