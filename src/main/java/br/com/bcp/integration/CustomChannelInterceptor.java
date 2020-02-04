328804575e4665a4ad9b475611d7ee59522d93cd
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-02-04 19:13:02 -0300

    interceptor

---
+++ src/main/java/br/com/bcp/integration/CustomChannelInterceptor.java
@@ -0,0 +1,16 @@
+package br.com.bcp.integration;
+
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
+
+public class CustomChannelInterceptor implements ChannelInterceptor {
+
+	@Override
+	public Message<?> preSend(Message<?> message, MessageChannel channel) {
+		Message<?> msg = MessageBuilder.withPayload(message.getPayload().toString() + " Message Intercepted").build();
+		return ChannelInterceptor.super.preSend(msg, channel);
+	}
+
+}
