c048d9c2834f2d3f630cd8bc35716b06d497a566
bruno.proenca <bruno.proenca@jp010nt.synchro.com.br>
2020-03-20 18:51:43 -0300

    file gateway

--- src/main/java/br/com/bcp/integration/FilePrinter.java
+++ src/main/java/br/com/bcp/integration/FilePrinter.java
@@ -2,15 +2,19 @@ package br.com.bcp.integration;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FilePrinter {
 
+	public void print (String file) {
+		System.out.println("Invoking the print method with a STRING");
+		System.out.println(file);
+	}
+	
 	public void print(File file) {
+		System.out.println("Invoking the print method with a FILE");
 		BufferedReader reader = null;
-		
 		try {
 			reader = new BufferedReader(new FileReader(file));
 			String line = "";
