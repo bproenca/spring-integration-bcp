4086ebe78bc24bb5f0131007f7aca425d4c7e5c6
bruno.proenca <bruno.proenca@jp010nt.synchro.com.br>
2020-03-20 13:38:22 -0300

    read file

--- src/main/java/br/com/bcp/integration/PersonDirectoryService.java
+++
@@ -1,9 +0,0 @@
-package br.com.bcp.integration;
-
-public class PersonDirectoryService {
-
-	public Person findNewPeople() {
-		System.out.println("Inbound adapter has polled this service");
-		return new Person("Bruno", "Proenca");
-	}
-}
--- src/main/java/br/com/bcp/integration/PersonRegistrationService.java
+++
@@ -1,8 +0,0 @@
-package br.com.bcp.integration;
-
-public class PersonRegistrationService {
-
-	public void registerEmail (Person person) {
-		System.out.println("Email created to: " + person.getFirstName());
-	}
-}
